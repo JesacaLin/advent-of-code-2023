ba0f051cd2b2f3c3d5e2bfad7ff9d583911aa3cc
JesacaLin <JesacaLin@gmail.com>
2023-12-09 18:08:21 -0500

    Day Two

--- AdventOfCodeDay2.java
+++ AdventOfCodeDay2.java
@@ -3,6 +3,8 @@ import java.net.HttpURLConnection;
 import java.util.Scanner;
 import java.io.InputStream;
 import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 //PLEASE REPLACE BROILER PLATE FUNCTION NAME AND INPUT LINK!
@@ -18,10 +20,46 @@ public class AdventOfCodeDay2 {
         connection.setRequestProperty("Cookie", "session=" + sessionCookie);
         InputStream in = connection.getInputStream();
         Scanner scanner = new Scanner(in);
-    
+        
+        //split the input by : to separate game number and game result
+
+        Map<String, Integer> thresholds = new HashMap<>();
+        thresholds.put("red", 12);
+        thresholds.put("green", 13);
+        thresholds.put("blue", 14);
+
+        int gameTotal = 0;
+
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            System.out.println(line);
+            String[] splitInput = line.split(":");
+            String gameTitle = splitInput[0];
+            String[] splitTitle = gameTitle.split(" ");
+            int gameNumber = Integer.parseInt(splitTitle[1]);
+   
+            String gameBody = splitInput[1];
+            String[] games = gameBody.split(";");
+            boolean exceedsThreshold = false;
+            for (String game : games) {
+                String[] colorCounts = game.split(",");
+                for (String colorCount : colorCounts) {
+                    String[] parts = colorCount.trim().split(" ");
+                    int count = Integer.parseInt(parts[0]);
+                    String color = parts[1];
+                    if (count > thresholds.get(color)) {
+                        exceedsThreshold = true;
+                        break;
+                    }
+                }
+                if (exceedsThreshold) {
+                    break;
+                }
+            }
+            if (!exceedsThreshold) {
+                gameTotal += gameNumber;
+            }
+            System.out.println("Game number :" + gameNumber);
+            System.out.println("Game Total :" + gameTotal);
         }
         scanner.close();
     }
