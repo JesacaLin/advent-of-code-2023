b83bb42b82426435711ca94a555e9837ed8e43b9
JesacaLin <JesacaLin@gmail.com>
2023-12-09 18:46:47 -0500

    update broiler plate

--- AdventOfCode_BroilerPlate.java
+++ AdventOfCode_BroilerPlate.java
@@ -1,11 +1,44 @@
+// import java.net.URL;
+// import java.net.HttpURLConnection;
+// import java.util.Scanner;
+// import java.io.InputStream;
+// import java.io.FileInputStream;
+// import java.util.Properties;
+
+// //PLEASE REPLACE BROILER PLATE FUNCTION NAME AND INPUT LINK!
+
+// public class AdventOfCode_BroilerPlate {
+//     public static void main(String[] args) throws Exception {
+//         Properties prop = new Properties();
+//         prop.load(new FileInputStream(".env"));
+//         String sessionCookie = prop.getProperty("SESSION_COOKIE");
+
+//         URL url = new URL(prop.getProperty("DAY_TWO_INPUT_URL"));
+//         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//         connection.setRequestProperty("Cookie", "session=" + sessionCookie);
+//         InputStream in = connection.getInputStream();
+//         Scanner scanner = new Scanner(in);
+//         int counter = 0;
+//         while (scanner.hasNextLine()) {
+//             String line = scanner.nextLine();
+//             counter++;
+//             System.out.println(counter);
+//         }
+//         scanner.close();
+//     }
+// }
+
+
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.util.Scanner;
 import java.io.InputStream;
 import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
-//PLEASE REPLACE BROILER PLATE FUNCTION NAME AND INPUT LINK!
+//PLEASE REPLACE BROILER PLATE FUNCTION NAME!
 
 public class AdventOfCode_BroilerPlate {
     public static void main(String[] args) throws Exception {
@@ -13,17 +46,20 @@ public class AdventOfCode_BroilerPlate {
         prop.load(new FileInputStream(".env"));
         String sessionCookie = prop.getProperty("SESSION_COOKIE");
 
-        URL url = new URL("https://adventofcode.com/2023/day/1/input");
+        //PLEASE REPLACE INPUT LINK!
+
+        URL url = new URL(prop.getProperty("DAY_TWO_INPUT_URL"));
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestProperty("Cookie", "session=" + sessionCookie);
-        InputStream in = connection.getInputStream();
-        Scanner scanner = new Scanner(in);
-        int counter = 0;
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            counter++;
-            System.out.println(counter);
+
+        try (InputStream in = connection.getInputStream(); Scanner scanner = new Scanner(in)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(lin);
+            }
         }
-        scanner.close();
     }
+
+    private static int processGame(String line) {
+        //here is a possible helper function.
 }
